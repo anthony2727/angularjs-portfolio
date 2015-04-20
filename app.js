0682caf89e7eb346c8047a99547b001f74ef8746
Anthony Rodriguez <anthonyrodriguez.itt@gmail.com>
2015-04-20 00:16:47 +0000

    ui-route implemented

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 
-angular.module('portfolioApp',[])
+angular.module('portfolioApp',['ngRoute', 'ui.router'])
+
+.config(function($stateProvider){
+
+	$stateProvider.state('lists', {
+		url: '/:lists',
+		templateUrl: 'views/lists.html'
+	})
+	
+})
